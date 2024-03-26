6604b96901997524960d1a547af30c19e91e258c
LavP-PY <161367819+LavP-PY@users.noreply.github.com>
2024-03-26 20:41:21 +0300

    Add files via upload

---
+++ data_download.py
@@ -0,0 +1,26 @@
+import yfinance as yf
+
+
+def fetch_stock_data(ticker, period='1mo'):
+    stock = yf.Ticker(ticker)
+    data = stock.history(period=period)
+    return data
+
+
+def add_moving_average(data, window_size=5):
+    data['Moving_Average'] = data['Close'].rolling(window=window_size).mean()
+    all_period = len(data) - 1
+    data['Average_period'] = data['Close'].sum() / all_period
+    return data
+
+
+# def add_average_all_period(data):
+#     window_size = len(data)
+#     data['Average_period'] = data['Close'].rolling(window=window_size).mean()
+#     return data
+
+
+# af = fetch_stock_data(ticker="GOOGL", period='6mo')
+# # # print(af)
+# add = add_moving_average(data=af)
+# print(add)
