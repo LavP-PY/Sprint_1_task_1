946f98db7ee5c1392ecc4e776aafff975f9826fa
LavP-PY <161367819+LavP-PY@users.noreply.github.com>
2024-03-28 11:56:45 +0300

    data_download.py

--- data_download.py
+++ data_download.py
@@ -9,18 +9,6 @@ def fetch_stock_data(ticker, period='1mo'):
 
 def add_moving_average(data, window_size=5):
     data['Moving_Average'] = data['Close'].rolling(window=window_size).mean()
-    all_period = len(data) - 1
+    all_period = len(data)
     data['Average_period'] = data['Close'].sum() / all_period
     return data
-
-
-# def add_average_all_period(data):
-#     window_size = len(data)
-#     data['Average_period'] = data['Close'].rolling(window=window_size).mean()
-#     return data
-
-
-# af = fetch_stock_data(ticker="GOOGL", period='6mo')
-# # # print(af)
-# add = add_moving_average(data=af)
-# print(add)
