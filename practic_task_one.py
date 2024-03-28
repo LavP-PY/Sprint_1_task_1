93085467f482597b12e7abf1f16c8ff644dbccda
LavP-PY <161367819+LavP-PY@users.noreply.github.com>
2024-03-28 11:55:14 +0300

    practic_task_one.py

--- practic_task_one.py
+++ practic_task_one.py
@@ -16,40 +16,7 @@ import data_download as dd
 import matplotlib.pyplot as plt
 import pandas as pd
 
-ticker = input("Введите тикер акции (например, «AAPL» для Apple Inc):")
-period = input("Введите период для данных (например, '1mo' для одного месяца): ")
-
-stock_data = dd.fetch_stock_data(ticker, period)
-stock_data = dd.add_moving_average(stock_data)
-
-def calculate_and_display_average_price(data, ticker, period, filename=None):
-    plt.figure(figsize=(10, 6)) # размеры (ширина и высота) диаграммы
-
-    if 'Date' not in data:
-        if pd.api.types.is_datetime64_any_dtype(data.index):
-            dates = data.index.to_numpy()
-            plt.plot(dates, data['Close'].values, label='Close Price')
-            plt.plot(dates, data['Moving_Average'].values, label='Moving Average')
-            plt.plot(dates, data['Average_period'].values, label='Average all period')
-        else:
-            print("Информация о дате отсутствует или не имеет распознаваемого формата.")
-            return
-    else:
-        if not pd.api.types.is_datetime64_any_dtype(data['Date']):
-            data['Date'] = pd.to_datetime(data['Date'])
-        plt.plot(data['Date'], data['Close'], label='Close Price')
-        plt.plot(data['Date'], data['Moving_Average'], label='Moving Average')
-        plt.plot(data['Date'], data['Average_period'], label='Average all period')
-
-    plt.title(f"{ticker} Цена акций с течением времени")
-    plt.xlabel("Дата")
-    plt.ylabel("Цена")
-    plt.legend()
-
-    if filename is None:
-        filename = f"{ticker}_{period}_stock_price_chart.png"
-
-    plt.savefig(filename)
-    print(f"График сохранен как {filename}")
-
-averenge = calculate_and_display_average_price(data=stock_data, ticker=ticker, period=period)
+def calculate_and_display_average_price(data):
+    all_period = len(data)
+    average_period = data['Close'].sum() / all_period
+    return average_period
