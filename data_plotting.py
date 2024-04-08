dea0c8cf879a4918ed9f11e0a21ac233980f6da3
LavP-PY <161367819+LavP-PY@users.noreply.github.com>
2024-04-08 15:59:12 +0300

    data_plotting.py

--- data_plotting.py
+++ data_plotting.py
@@ -1,15 +1,15 @@
 import matplotlib.pyplot as plt
-import pandas as pd
 
 
 def create_and_save_plot(data, ticker, period, filename=None):
-    plt.figure(figsize=(10, 6)) # размеры (ширина и высота) диаграммы
-
+    plt.figure(figsize=(12, 18)) # размеры (ширина и высота) диаграммы
+    plt.subplot(3, 1, 1)
     if 'Date' not in data:
         if pd.api.types.is_datetime64_any_dtype(data.index):
             dates = data.index.to_numpy()
             plt.plot(dates, data['Close'].values, label='Close Price')
             plt.plot(dates, data['Moving_Average'].values, label='Moving Average')
+
         else:
             print("Информация о дате отсутствует или не имеет распознаваемого формата.")
             return
@@ -23,6 +23,46 @@ def create_and_save_plot(data, ticker, period, filename=None):
     plt.xlabel("Дата")
     plt.ylabel("Цена")
     plt.legend()
+    # plt.grid(True)
+
+    plt.subplot(3, 1, 2)
+    plt.title("RSI")
+    if 'Date' not in data:
+        if pd.api.types.is_datetime64_any_dtype(data.index):
+            dates = data.index.to_numpy()
+            plt.plot(dates, data['RSI_ema'].values, label='RSI EMA')
+            plt.plot(dates, data['RSI_sma'].values, label='RSI SMA')
+
+        else:
+            print("Информация о дате отсутствует или не имеет распознаваемого формата.")
+            return
+    else:
+        if not pd.api.types.is_datetime64_any_dtype(data['Date']):
+            data['Date'] = pd.to_datetime(data['Date'])
+        plt.plot(data['Date'], data['RSI_ema'], label='RSI EMA')
+        plt.plot(data['Date'], data['RSI_sma'], label='RSI SMA')
+    plt.legend()
+    plt.grid(True)
+
+    plt.subplot(3, 1, 3)
+    plt.title("MACD")
+    if 'Date' not in data:
+        if pd.api.types.is_datetime64_any_dtype(data.index):
+            dates = data.index.to_numpy()
+            plt.plot(dates, data['MCAD_main'].values, label='MCAD basic')
+            plt.plot(dates, data['MCAD_signal'].values, label='MCAD signal')
+
+        else:
+            print("Информация о дате отсутствует или не имеет распознаваемого формата.")
+            return
+    else:
+        if not pd.api.types.is_datetime64_any_dtype(data['Date']):
+            data['Date'] = pd.to_datetime(data['Date'])
+        plt.plot(data['Date'], data['MCAD_main'], label='MCAD basic')
+        plt.plot(data['Date'], data['MCAD_signal'], label='MCAD signal')
+    plt.legend()
+    plt.grid(True)
+
 
     if filename is None:
         filename = f"{ticker}_{period}_stock_price_chart.png"
