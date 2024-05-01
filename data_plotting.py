114f2693aa7041b7f996d2d0dfda03dee428ab45
LavP-PY <161367819+LavP-PY@users.noreply.github.com>
2024-05-01 20:21:55 +0300

    data_plotting.py

--- data_plotting.py
+++ data_plotting.py
@@ -1,84 +1,77 @@
-import matplotlib.pyplot as plt
-import pandas as pd
-import plotly.graph_objects as go
+import yfinance as yf
 
 
-def create_and_save_plot(data, ticker, period, choice_style=None, filename=None):
-    if choice_style is not None:
-        plt.style.use(choice_style)
+def fetch_stock_data(ticker, period):
+    """
+    Принимает имя тикера и период, введённые пользователем.
+    :param ticker:
+    :param period:
+    :return: Возвращает таблицу с данными об акции
+    """
+    stock = yf.Ticker(ticker)
+    data = stock.history(period=period)
+    return data
 
-    plt.figure(figsize=(12, 18)) # размеры (ширина и высота) диаграммы
-    plt.subplot(3, 1, 1)
+def calculate_and_display_average_price_rsi(data, period=14, ema=True):
+    """
+    Принимает таблицу с данными акции. Рассчитывает (по скользящим значениям цены закрытия) и возвращает индекс RSI
+    :param data:
+    :param period:
+    :param ema:
+    :return: RSI (индекс относительной силы)
+    """
+    close_delta = data['Close'].diff()
+    # Делаем две серии: одну для низких закрытий и одну для высоких закрытий
+    up = close_delta.clip(lower=0)
+    down = -1 * close_delta.clip(upper=0)
 
-    if 'Date' not in data:
-        if pd.api.types.is_datetime64_any_dtype(data.index):
-            dates = data.index.to_numpy()
-            plt.plot(dates, data['Close'].values, label='Close Price')
-            plt.plot(dates, data['Moving_Average'].values, label='Moving Average')
-            plt.plot(dates, data['Standart_deviation_list'].values, label='Standart deviation')
-
-        else:
-            print("Информация о дате отсутствует или не имеет распознаваемого формата.")
-            return
+    if ema == True:
+        # Использование экспоненциальной скользящей средней
+        ma_up = up.ewm(com=period - 1, adjust=True, min_periods=period).mean()
+        ma_down = down.ewm(com=period - 1, adjust=True, min_periods=period).mean()
     else:
-        if not pd.api.types.is_datetime64_any_dtype(data['Date']):
-            data['Date'] = pd.to_datetime(data['Date'])
-        plt.plot(data['Date'], data['Close'], label='Close Price')
-        plt.plot(data['Date'], data['Moving_Average'], label='Moving Average')
-        plt.plot(data['Date'], data['Standart_deviation_list'], label='Standart deviation')
+        # Использование простой скользящей средней
+        ma_up = up.rolling(window=period).mean()
+        ma_down = down.rolling(window=period).mean()
 
-    plt.title(f"{ticker} Цена акций с течением времени")
-    plt.xlabel("Дата")
-    plt.ylabel("Цена")
-    plt.legend(loc='center left')
-    # plt.grid(True)
-
-    plt.subplot(3, 1, 2)
-    plt.title("RSI")
-    if 'Date' not in data:
-        if pd.api.types.is_datetime64_any_dtype(data.index):
-            dates = data.index.to_numpy()
-            plt.plot(dates, data['RSI_ema'].values, label='RSI EMA')
-            plt.plot(dates, data['RSI_sma'].values, label='RSI SMA')
-
-        else:
-            print("Информация о дате отсутствует или не имеет распознаваемого формата.")
-            return
-    else:
-        if not pd.api.types.is_datetime64_any_dtype(data['Date']):
-            data['Date'] = pd.to_datetime(data['Date'])
-        plt.plot(data['Date'], data['RSI_ema'], label='RSI EMA')
-        plt.plot(data['Date'], data['RSI_sma'], label='RSI SMA')
-    plt.legend()
-    plt.grid(True)
-
-    plt.subplot(3, 1, 3)
-    plt.title("MACD")
-    if 'Date' not in data:
-        if pd.api.types.is_datetime64_any_dtype(data.index):
-            dates = data.index.to_numpy()
-            plt.plot(dates, data['MCAD_main'].values, label='MCAD basic')
-            plt.plot(dates, data['MCAD_signal'].values, label='MCAD signal')
-
-        else:
-            print("Информация о дате отсутствует или не имеет распознаваемого формата.")
-            return
-    else:
-        if not pd.api.types.is_datetime64_any_dtype(data['Date']):
-            data['Date'] = pd.to_datetime(data['Date'])
-        plt.plot(data['Date'], data['MCAD_main'], label='MCAD basic')
-        plt.plot(data['Date'], data['MCAD_signal'], label='MCAD signal')
-    plt.legend(loc='upper left')
-    plt.grid(True)
+    rs = ma_up / ma_down
+    rsi = 100 - (100 / (1 + rs))
+    return rsi
 
 
-    if filename is None:
-        filename = f"{ticker}_{period}_stock_price_chart__{choice_style}.png"
+def add_moving_average(data, window_size=5):
+    """
+    Рассчитывает и добавляет в таблицу данные по акции для последующей их визуализации:
+        скользящая средняя, индикаторы RSI SMA и RSI EMA, индикаторы MACD (стандартная и сигнальная),
+        стандарт отклонения выборки.
+    Возвращает таблицу с новыми данными.
+    :param data:
+    :param window_size: стандартное значение для расчета скользящей средней
+    :return: таблица data
+    """
+    # Расчет скользящей средней
+    data['Moving_Average'] = data['Close'].rolling(window=window_size).mean()
 
-    plt.savefig(filename)
-    print(f"График сохранен как {filename}")
-    
-    # Подключаем альтернативную библиотеку для построения графиков
-    date_time = data.index.to_numpy()
-    fig = go.Figure([go.Scatter(x=date_time, y=data['Close'].values)])
-    fig.show()
+    # RSI
+    data['RSI_ema'] = calculate_and_display_average_price_rsi(data=data, ema=True)
+    data['RSI_sma'] = calculate_and_display_average_price_rsi(data=data, ema=False)
+
+    # MCAD
+    ema_fast = 12
+    ema_slow = 26
+    sma_signal = 9
+    # Для вычисления экспоненциально взвешенной скользящей средней (EMA) за определенное количество предыдущих периодов используем функцию pandas.DataFrame.ewm()
+    # min_periods в методе ewm, чтобы не рассчитывать значения в первых периодах  временного ряда, который основан на неполных данных
+    data['Moving_Average_fast'] = data['Close'].ewm(com=ema_fast - 1, adjust=True, min_periods=ema_fast).mean()
+    data['Moving_Average_slow'] = data['Close'].ewm(com=ema_slow - 1, adjust=True, min_periods=ema_slow).mean()
+    data['MCAD_main'] = data['Moving_Average_fast'] - data['Moving_Average_slow']
+    data['MCAD_signal'] = data['MCAD_main'].rolling(window=sma_signal).mean()
+
+    # Расчет стандарта отклонения выборки по списку (calculate standard deviation of list)
+    data['Standart_deviation_list'] = data['Close'].rolling(window=window_size).std()
+
+    # Расчет для модуля practic_task_one_v2.py
+    all_period = len(data)
+    data['Average_period'] = data['Close'].sum() / all_period
+    return data
+
